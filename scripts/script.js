001830b3b0758f0fc512a5c355e67b13ecef47ff
JacksonTheGeologist <jbjakeway@gmail.com>
2023-01-18 20:32:14 -0500

    fixing touch

--- scripts/script.js
+++ scripts/script.js
@@ -139,7 +139,7 @@ function togglePlayers() {
   // numPlayersTextP.textContent = `${numPlayers} players`;
   numPlayersTextP.textContent = `${numPlayers} players`;
 }
-numPlayersToggle.addEventListener(touchEvent, togglePlayers);
+numPlayersToggle.addEventListener(touchEvent, togglePlayers, false);
 
 gameStartBtn.addEventListener(touchEvent, function () {
   headerInfoEl.classList.add('opacity-transition');
